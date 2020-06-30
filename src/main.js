21d2990e981018a61138c28fb55c3e6315d462cb
Yong <395439385@qq.com>
2020-06-30 14:43:52 +0800

    上传移动端音乐项目

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
+
+Vue.use(ElementUI)
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
